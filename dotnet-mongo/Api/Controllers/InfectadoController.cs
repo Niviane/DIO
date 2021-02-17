1aae82ea7181ef519c42d372885b6b2ea5474b9c
niviane nucci <niviane.nucci@gmail.com>
2021-02-17 00:47:42 -0300

    Restful api 3.1.406 MongoDB

---
+++ dotnet-mongo/Api/Controllers/InfectadoController.cs
@@ -0,0 +1,58 @@
+﻿using Api.Data.Collections;
+using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+            
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+            
+            return Ok(infectados);
+        }
+
+        [HttpPut]
+        public ActionResult AtualizarInfectado([FromBody] InfectadoDto dto)
+        {
+            //O ideal era passar um  Id no parâmetro
+            //atualiza uma única propriedade, para atualizar o objeto todo usar Replace. A iédia do Path é atualizar trecho, o mais indicado nesse caso.
+            _infectadosCollection.UpdateOne(Builders<Infectado>.Filter.Where(_ => _.DataNascimento = dto.DataNascimento), Builders<Infectado>.Update.Set("Sexo" , dto.Sexo));
+
+            return Ok("Atualizado com sucesso");
+        }
+
+        [HttpDelete("{DataNasc}")]
+        public ActionResult Delete(Datetime DataNasc)
+        {
+            //O ideal era passar um  Id no parâmetro
+            _infectadosCollection.UpdateOne(Builders<Infectado>.Filter.Where(_ => _.DataNascimento = DataNasc));
+
+            return Ok("Registro foi apagado");
+        }
+    }
+}
